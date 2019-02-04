6d43f2dad3f951fcd63c3c554b9fe621898364ae
RCaracuel <rosa.caracuel@gmail.com>
2019-02-04 20:25:50 +0100

    Creacion de la clase EmpresaAlquilerVehiculos

---
+++ src/alquilervehiculos/EmpresaAlquilerVehiculos.java
@@ -0,0 +1,132 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package alquilervehiculos;
+
+/**
+ *
+ * @author Rosa
+ */
+public class EmpresaAlquilerVehiculos {
+
+    // Atributos de la empresa
+    private String cif;
+    private String nombre;
+    private String paginaWeb;
+    /* Atributos para controlar el total de clientes que tiene la
+empresa y array de almacenamiento para los objetos Cliente */
+    private int totalClientes;
+    private Cliente[] clientes;
+    /* Atributos para controlar el total de vehiculos disponibles en
+alquiler que tiene la empresa y array de almacenamiento para los objetos
+Vehiculo */
+    private int totalVehiculos;
+    private Vehiculo[] vehiculos;
+    /* Atributos para controlar el histórico de alquileres: total de
+alquileres realizados y array de almacenamiento para los objetos
+VehiculoAlquilado */
+    private int totalAlquileres;
+    private VehiculoAlquilado[] alquileres;
+// se omiten los métodos ‘get’ y ‘set’ de la clase
+/* Constructor parametrizado donde se establece que el total de clientes
+será 50, igual que el total de vehiculos disponibles. El histórico de
+vehiculos puede contener hasta 100 elementos */
+   public EmpresaAlquilerVehiculos(String cif, String nombre, String paginaWeb) {
+        this.cif = cif;
+        this.nombre = nombre;
+        this.paginaWeb = paginaWeb;
+// Incialmente no hay clientes creados en la empresa
+        this.totalClientes = 0;
+        this.clientes = new Cliente[50]; // apuntan a null
+// Incialmente no hay vehiculos creados en la empresa
+        this.totalVehiculos = 0;
+        this.vehiculos = new Vehiculo[50]; // apuntan a null
+// Incialmente no hay histórico de alquileres en la empresa
+        this.totalAlquileres = 0;
+        this.alquileres = new VehiculoAlquilado[100]; // apuntan a null
+    }
+   
+   //getters y setters
+
+    public String getCif() {
+        return cif;
+    }
+
+    public void setCif(String cif) {
+        this.cif = cif;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getPaginaWeb() {
+        return paginaWeb;
+    }
+
+    public void setPaginaWeb(String paginaWeb) {
+        this.paginaWeb = paginaWeb;
+    }
+
+    public int getTotalClientes() {
+        return totalClientes;
+    }
+
+    public void setTotalClientes(int totalClientes) {
+        this.totalClientes = totalClientes;
+    }
+
+    public Cliente[] getClientes() {
+        return clientes;
+    }
+
+    public void setClientes(Cliente[] clientes) {
+        this.clientes = clientes;
+    }
+
+    public int getTotalVehiculos() {
+        return totalVehiculos;
+    }
+
+    public void setTotalVehiculos(int totalVehiculos) {
+        this.totalVehiculos = totalVehiculos;
+    }
+
+    public Vehiculo[] getVehiculos() {
+        return vehiculos;
+    }
+
+    public void setVehiculos(Vehiculo[] vehiculos) {
+        this.vehiculos = vehiculos;
+    }
+
+    public int getTotalAlquileres() {
+        return totalAlquileres;
+    }
+
+    public void setTotalAlquileres(int totalAlquileres) {
+        this.totalAlquileres = totalAlquileres;
+    }
+
+    public VehiculoAlquilado[] getAlquileres() {
+        return alquileres;
+    }
+
+    public void setAlquileres(VehiculoAlquilado[] alquileres) {
+        this.alquileres = alquileres;
+    }
+
+    @Override
+    public String toString() {
+        return "EmpresaAlquilerVehiculos{" + "cif=" + cif + ", nombre=" + nombre + ", paginaWeb=" + paginaWeb + ", totalClientes=" + totalClientes + ", clientes=" + clientes + ", totalVehiculos=" + totalVehiculos + ", vehiculos=" + vehiculos + ", totalAlquileres=" + totalAlquileres + ", alquileres=" + alquileres + '}';
+    }
+   
+    
+    
+}
