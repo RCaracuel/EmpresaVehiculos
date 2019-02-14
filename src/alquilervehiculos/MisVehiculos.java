0673d47c21abfd1d24ce5f636e334c83f4a6dc5d
RCaracuel <rosa.caracuel@gmail.com>
2019-02-14 12:24:18 +0100

    Llamar a los métodos en el main

--- src/alquilervehiculos/MisVehiculos.java
+++ src/alquilervehiculos/MisVehiculos.java
@@ -5,6 +5,8 @@
  */
 package alquilervehiculos;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Rosa
@@ -13,41 +15,69 @@ public class MisVehiculos {
 
     public static void main(String[] args) {
 
-        // la instancia easydrive de la clase EmpresaAlquilerVehiculos
-        EmpresaAlquilerVehiculos easydrive = new EmpresaAlquilerVehiculos("A-28-187189", "easy drive",
-                "www.easydrive.com");
-// registro de los clientes de la empresa
-        easydrive.registrarCliente(new Cliente("X5618927C",
-                "Juan", "González López"));
-
-        easydrive.registrarCliente(new Cliente("Z7568991Y",
-                "Luis", "Fernández Gómez"));
-
-// registro de los vehículos de la empresa
-        easydrive.registrarVehiculo(new Vehiculo("4060 TUR", "Skoda",
-                "Fabia", "Blanco", 90.0, false));
-
-        easydrive.registrarVehiculo(new Vehiculo("4070 DEP", "Ford", "Mustang",
-                "Rojo", 150.0, true));
-
-        easydrive.registrarVehiculo(new Vehiculo("4080 TUR", "VW", "GTI", "Azul", 110.0, false));
-
-        easydrive.registrarVehiculo(new Vehiculo("4090 TUR", "SEAT", "Ibiza",
-                "Blanco", 90.0, false));
-
-        easydrive.registrarVehiculo(new Vehiculo("4100 FUR", "Fiat", "Ducato",
-                "Azul", 80.0, true));
-// imprime la relación de clientes de easydrive
-        easydrive.imprimirClientes();
-// imprime el catálogo de vehículos de easydrive
-        easydrive.imprimirVehiculos();
+//        // la instancia easydrive de la clase EmpresaAlquilerVehiculos
+//        EmpresaAlquilerVehiculos easydrive = new EmpresaAlquilerVehiculos("A-28-187189", "easy drive",
+//                "www.easydrive.com");
+//// registro de los clientes de la empresa
+//        easydrive.registrarCliente(new Cliente("X5618927C",
+//                "Juan", "González López"));
+//
+//        easydrive.registrarCliente(new Cliente("Z7568991Y",
+//                "Luis", "Fernández Gómez"));
+//
+//// registro de los vehículos de la empresa
+//        easydrive.registrarVehiculo(new Vehiculo("4060 TUR", "Skoda",
+//                "Fabia", "Blanco", 90.0, false));
+//
+//        easydrive.registrarVehiculo(new Vehiculo("4070 DEP", "Ford", "Mustang",
+//                "Rojo", 150.0, true));
+//
+//        easydrive.registrarVehiculo(new Vehiculo("4080 TUR", "VW", "GTI", "Azul", 110.0, false));
+//
+//        easydrive.registrarVehiculo(new Vehiculo("4090 TUR", "SEAT", "Ibiza",
+//                "Blanco", 90.0, false));
+//
+//        easydrive.registrarVehiculo(new Vehiculo("4100 FUR", "Fiat", "Ducato",
+//                "Azul", 80.0, true));
+//// imprime la relación de clientes de easydrive
+//        easydrive.imprimirClientes();
+//// imprime el catálogo de vehículos de easydrive
+//        easydrive.imprimirVehiculos();
+//        
+//        
+//        Vehiculo uno= new Vehiculo();
+//        
+//        System.out.println(uno);
+//        Cliente clUno= new Cliente();
+//        
+//        System.out.println(clUno);
+//        
+//        //Creamos empresa
+//       
+        Scanner teclado= new Scanner(System.in);
+        EmpresaAlquilerVehiculos prueba= new EmpresaAlquilerVehiculos("9889u7", "Derp", "www.hellomadafaka.com");
         
+        prueba.rellenarVehiculos();
+        prueba.rellenarClientes();
+        prueba.imprimirClientes();
+        System.out.println("------------------");
+        prueba.imprimirVehiculos();
+        prueba.burbujaCliente();
+        prueba.burbujaVehiculos();
+        prueba.imprimirClientes();
+        System.out.println("------------------");
+        prueba.imprimirVehiculos();
         
-        Vehiculo uno= new Vehiculo();
+        System.out.println("Introduzca Nif a buscar:");
+        String aux=teclado.nextLine();
         
-        System.out.println(uno);
-        Cliente clUno= new Cliente();
+        prueba.busquedaNif(aux);
         
-        System.out.println(clUno);
+        if(prueba.busquedaNif(aux)==-1){
+            System.out.println("No se ha encontrado");
+        }else{
+            System.out.println("Lo hemos encontrado en la posición "+prueba.busquedaNif(aux));
+        }
+        
     }
 }
